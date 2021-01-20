b961d69df5054ce38563101979340c1a9f11506f
raghu <raghumina11@gmail.com>
2021-01-20 11:03:59 +0530

    Update MineCraft_Python.py

--- MineCraft_Python.py
+++ MineCraft_Python.py
@@ -4,4 +4,12 @@
 
 # Tool and Languages used: Python, PyCharm, GitHub
 
-import ursina
+# lets import librariers form ursina
+
+from ursina import *    #
+
+
+# we have to create an instance for the game for example board
+app = Ursina()           # created an instance named app
+app.run()                # ran it
+
