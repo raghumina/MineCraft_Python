419337db210c12dd7a160fb2f4959f6eec1c800a
raghu <raghumina11@gmail.com>
2021-01-20 11:08:43 +0530

    Update MineCraft_Python.py

--- MineCraft_Python.py
+++ MineCraft_Python.py
@@ -4,12 +4,20 @@
 
 # Tool and Languages used: Python, PyCharm, GitHub
 
-# lets import librariers form ursina
+# lets import libraries form Ursina
 
-from ursina import *    #
-
+from ursina import *  #
 
 # we have to create an instance for the game for example board
-app = Ursina()           # created an instance named app
-app.run()                # ran it
+app = Ursina()  # created an instance named app
+
+# we have to create an entity
+# in Ursina entity's are like building blocks of game they can be anything for example
+# Squares, Boxes, Sounds,
+# We can create them according to our need and can modify them also.
+
+
+
+app.run()  # ran it
+# by running app we can see the game board or window and frames.
 
