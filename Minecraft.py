7eb825a5ddd46a29366d42f0bde8d21750f875e0
raghu <raghumina11@gmail.com>
2021-01-20 13:23:15 +0530

    Update Minecraft.py

--- Minecraft.py
+++ Minecraft.py
@@ -4,27 +4,25 @@
 
 from ursina import *
 
+
 # Minecraft is a Voxel based game
 # Voxel are
 class Voxel(Button):
     def __init__(self):
         super().__init__(
-            parent = scene,
-            position = (0,0,0),
-            model = 'cube',
-            origin_y = 0.5,
-            texture  = 'white_cube',
-            color= color.white,
-            highlight_color = 'color.lime'
+            parent=scene,
+            position=(0, 0, 0),
+            model='cube',
+            origin_y=0.5,
+            texture='white_cube',
+            color=color.white,
+            highlight_color='color.lime'
         )
 
 
-
-
 app = Ursina()
-for x in range(8):           # creating buttons here
+for x in range(8):  # creating buttons here
     for z in range(8):
         voxel = Voxel()
         
-
 app.run()
